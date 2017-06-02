780c15c49d706d8c061f3b67c3a1b875e256d4cb
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-06-02 22:07:02 +0200

    add native api client

---
+++ src/ProGet.Net/Native/ProGetClient.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Flurl.Http;
+
+// ReSharper disable CheckNamespace
+
+namespace ProGet.Net
+{
+    public partial class ProGetClient
+    {
+        private IFlurlClient GetNativeApiClient(string path, object queryParamValues = null) => GetApiClient("/api/json")
+            .AppendPathSegment(path)
+            .SetQueryParams(queryParamValues)
+            .AllowAnyHttpStatus();
+
+        private async Task<TResult> ExecuteNativeApiMethodAsync<TResult>(string nativeApiMethodName, IDictionary<string, object> parameters = null)
+        {
+            var response = await GetNativeApiClient(nativeApiMethodName, parameters)
+                .GetJsonAsync<TResult>()
+                .ConfigureAwait(false);
+
+            return response;
+        }
+
+        private async Task<bool> ExecuteNativeApiMethodAsync(string nativeApiMethodName, IDictionary<string, object> parameters = null)
+        {
+            var response = await GetNativeApiClient(nativeApiMethodName, parameters)
+                .GetAsync()
+                .ConfigureAwait(false);
+
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
