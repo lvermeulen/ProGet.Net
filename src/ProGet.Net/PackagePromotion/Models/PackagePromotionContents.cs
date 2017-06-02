cca59e6b95eed998bbe1e34103a8f3716eec2103
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-06-02 21:58:39 +0200

    add PackagePromotionContents

--- src/ProGet.Net/PackagePromotion/Models/PackagePromotionContents.cs
+++ src/ProGet.Net/PackagePromotion/Models/PackagePromotionContents.cs
@@ -1,6 +1,14 @@
-﻿namespace ProGet.Net.PackagePromotion.Models
+﻿// ReSharper disable InconsistentNaming
+
+namespace ProGet.Net.PackagePromotion.Models
 {
     public class PackagePromotionContents
     {
+        public string packageName { get; set; }  
+        public string groupName { get; set; }  
+        public string version { get; set; }  
+        public string fromFeed { get; set; }  
+        public string toFeed { get; set; }  
+        public string comments { get; set; } 
     }
 }
