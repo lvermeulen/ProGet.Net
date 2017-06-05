e10ada5b71a67922b2ae495b3f884bc269d89734
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-06-05 16:07:39 +0200

    add EventTypes_* (empty models)

---
+++ src/ProGet.Net/Native/EventTypes/ProGetClient.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ProGet.Net.Common;
+using ProGet.Net.Native.Models;
+
+// ReSharper disable InconsistentNaming
+// ReSharper disable CheckNamespace
+
+namespace ProGet.Net
+{
+    public partial class ProGetClient
+    {
+        /// <summary>
+        /// Gets all of the event types
+        /// </summary>
+        public async Task<IEnumerable<EventType>> EventTypes_GetEventTypesAsync() => 
+            await ExecuteNativeApiMethodAsync<IEnumerable<EventType>>(nameof(EventTypes_GetEventTypesAsync).WithoutAsyncSuffix()).ConfigureAwait(false);
+    }
+}
