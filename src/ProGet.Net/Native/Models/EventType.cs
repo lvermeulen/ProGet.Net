c46219c95a21218e4b2b694c551bd84064ee31d5
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-06-05 16:29:39 +0200

    add event type models

--- src/ProGet.Net/Native/Models/EventType.cs
+++ src/ProGet.Net/Native/Models/EventType.cs
@@ -1,6 +1,10 @@
-﻿namespace ProGet.Net.Native.Models
+﻿// ReSharper disable InconsistentNaming
+
+namespace ProGet.Net.Native.Models
 {
     public class EventType
     {
+        public string Event_Code { get; set; }
+        public string Event_Description { get; set; }
     }
 }
