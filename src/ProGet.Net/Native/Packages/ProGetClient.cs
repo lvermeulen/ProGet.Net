a6d453ccc710c7ed574844759826de450ff0e7ae
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-06-05 10:34:21 +0200

    add Packages_* (empty models)

---
+++ src/ProGet.Net/Native/Packages/ProGetClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ProGet.Net.Common;
+using ProGet.Net.Native.Models;
+
+// ReSharper disable InconsistentNaming
+// ReSharper disable CheckNamespace
+
+namespace ProGet.Net
+{
+    public partial class ProGetClient
+    {
+        /// <summary>
+        /// Gets a list of combined packages filtering by the specified criteria
+        /// </summary>
+        public async Task<IEnumerable<PackagesCombined>> Packages_GetPackagesAsync(
+            int packages_Count,
+            int feed_Id,
+            DateTime publishedBefore_Date)
+        {
+            var queryParamValues = QueryParamValues.From(
+                new NamedValue(nameof(packages_Count).Capitalize(), packages_Count),
+                new NamedValue(nameof(feed_Id).Capitalize(), feed_Id),
+                new NamedValue(nameof(publishedBefore_Date).Capitalize(), publishedBefore_Date) //TODO: verify DateTime
+            );
+
+            var response = await ExecuteNativeApiMethodAsync<IEnumerable<PackagesCombined>>(nameof(Packages_GetPackagesAsync).WithoutAsyncSuffix(), queryParamValues).ConfigureAwait(false);
+
+            return response; //TODO: inline
+        }
+    }
+}
