d70ef51c83ddb28aaaae74e0069a57dd942fa6ce
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-06-05 16:02:59 +0200

    add configuration models

--- src/ProGet.Net/Native/Models/Configuration.cs
+++ src/ProGet.Net/Native/Models/Configuration.cs
@@ -1,6 +1,10 @@
-﻿namespace ProGet.Net.Native.Models
+﻿// ReSharper disable InconsistentNaming
+
+namespace ProGet.Net.Native.Models
 {
     public class Configuration
     {
+        public string Key_Name { get; set; }
+        public string Value_Text { get; set; }
     }
 }
---
+++ test/ProGet.Net.Tests/Native/Configuration/ProGetClientShould.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace ProGet.Net.Tests
+{
+    public partial class ProGetClientShould
+    {
+        [Fact]
+        public async Task Configuration_GetConfigurationAsync()
+        {
+            var results = await _client.Configuration_GetConfigurationAsync();
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+    }
+}
+
