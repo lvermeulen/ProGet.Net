bf7e72da3815d31cb3c468f28437450038f91cda
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-06-10 19:17:35 +0200

    add connector models

--- src/ProGet.Net/Native/Models/Connector.cs
+++ src/ProGet.Net/Native/Models/Connector.cs
@@ -1,6 +1,23 @@
-﻿namespace ProGet.Net.Native.Models
+﻿// ReSharper disable InconsistentNaming
+
+namespace ProGet.Net.Native.Models
 {
     public class Connector
     {
+        public int Connector_Id { get; set; }
+        public string Connector_Name { get; set; }
+        public string Connector_Url { get; set; }
+        public string FeedType_Name { get; set; }
+        public string User_Name { get; set; }
+        public object Password_Bytes { get; set; }
+        public int Timeout_Period { get; set; }
+        public object Filter_Csv { get; set; }
+        public object IndexUpdated_Date { get; set; }
+        public string Status_Code { get; set; }
+        public object Package_Count { get; set; }
+        public object LastTest_Date { get; set; }
+        public int MetadataCache_Minutes { get; set; }
+        public int MetadataCache_Count { get; set; }
+        public bool MetadataCache_Enabled_Indicator { get; set; }
     }
 }
---
+++ test/ProGet.Net.Tests/Native/Connectors/ProGetClientShould.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace ProGet.Net.Tests
+{
+    public partial class ProGetClientShould
+    {
+        [Fact]
+        public async Task Connectors_GetConnectorsAsync()
+        {
+            var results = await _client.Connectors_GetConnectorsAsync(2);
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+    }
+}
+
