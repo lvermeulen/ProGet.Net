34c22706470645efe070257c6e1c14bb46e2b1a4
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-06-10 21:03:40 +0200

    add logmessages models

--- src/ProGet.Net/Native/Models/LogMessage.cs
+++ src/ProGet.Net/Native/Models/LogMessage.cs
@@ -1,11 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿// ReSharper disable InconsistentNaming
 
 namespace ProGet.Net.Native.Models
 {
     public class LogMessage
     {
+        public int LogMessage_Id { get; set; }
+        public bool Read_Indicator { get; set; }
+        public string Logged_Date { get; set; }
+        public string Message_Text { get; set; }
+        public object Category_Name { get; set; }
+        public int Message_Level { get; set; }
+        public string Details_Bytes { get; set; }
+        public object Feed_Id { get; set; }
+        public object Feed_Name { get; set; }
+        public object FeedType_Name { get; set; }
+        public object Connector_Id { get; set; }
+        public object Connector_Name { get; set; }
+        public object Connector_Url { get; set; }
+        public object ConnectorType_Name { get; set; }
     }
 }
---
+++ test/ProGet.Net.Tests/Native/LogMessages/ProGetClientShould.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace ProGet.Net.Tests
+{
+    public partial class ProGetClientShould
+    {
+        [Fact]
+        public async Task LogMessages_GetMessagesAsync()
+        {
+            var results = await _client.LogMessages_GetMessagesAsync(1000, true, null, 0);
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+    }
+}
+
