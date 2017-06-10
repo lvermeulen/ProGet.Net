4bf7930ef36f9e02789369c435c11412783c4c1b
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-06-10 21:52:35 +0200

    add event occurences tests

---
+++ test/ProGet.Net.Tests/Native/EventOccurrences/ProGetClientShould.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace ProGet.Net.Tests
+{
+    public partial class ProGetClientShould
+    {
+        [Fact]
+        public async Task EventOccurrences_GetOccurrencesAsync()
+        {
+            var results = await _client.EventOccurrences_GetOccurrencesAsync(2);
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+    }
+}
+
