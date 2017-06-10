324bd3849e772a1a83432357161d275cf12557d8
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-06-10 21:47:03 +0200

    add IndexingServers_* (empty models)

---
+++ src/ProGet.Net/Native/IndexingServers/ProGetClient.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ProGet.Net.Common;
+using ProGet.Net.Native.Models;
+
+// ReSharper disable InconsistentNaming
+// ReSharper disable CheckNamespace
+
+namespace ProGet.Net
+{
+    public partial class ProGetClient
+    {
+        /// <summary>
+        /// Deletes an indexing server
+        /// </summary>
+        public async Task<bool> IndexingServers_DeleteServerAsync(string server_Name)
+        {
+            var queryParamValues = QueryParamValues.From(
+                new NamedValue(nameof(server_Name).Capitalize(), server_Name)
+            );
+
+            return await ExecuteNativeApiMethodAsync(nameof(IndexingServers_DeleteServerAsync).WithoutAsyncSuffix(), queryParamValues).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Gets the indexing servers registered with ProGet
+        /// </summary>
+        public async Task<IEnumerable<IndexingServer>> IndexingServers_GetServersAsync() => 
+            await ExecuteNativeApiMethodAsync<IEnumerable<IndexingServer>>(nameof(IndexingServers_GetServersAsync).WithoutAsyncSuffix()).ConfigureAwait(false);
+    }
+}
---
+++ test/ProGet.Net.Tests/Native/IndexingServers/ProGetClientShould.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace ProGet.Net.Tests
+{
+    public partial class ProGetClientShould
+    {
+        [Fact]
+        public async Task IndexingServers_GetServersAsync()
+        {
+            var results = await _client.IndexingServers_GetServersAsync();
+
+            Assert.NotNull(results);
+            //Assert.NotEmpty(results);
+        }
+    }
+}
+
