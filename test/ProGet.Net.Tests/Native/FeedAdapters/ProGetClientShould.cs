4ca44a018f309cde0bbd545b33906720b5f5f3d0
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-06-11 06:48:49 +0200

    add feed adapters tests

---
+++ test/ProGet.Net.Tests/Native/FeedAdapters/ProGetClientShould.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace ProGet.Net.Tests
+{
+    public partial class ProGetClientShould
+    {
+        [Fact]
+        public async Task FeedAdapters_GetFeedAdaptersAsync()
+        {
+            var results = await _client.FeedAdapters_GetFeedAdaptersAsync(2);
+
+            Assert.NotNull(results);
+            //Assert.NotEmpty(results);
+        }
+    }
+}
+
