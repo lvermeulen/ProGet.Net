bfa09be8fca57663c9c83b3580769e9db900887b
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-06-11 07:36:09 +0200

    add feed retention rule models

--- src/ProGet.Net/Native/Models/FeedRetentionRuleExtended.cs
+++ src/ProGet.Net/Native/Models/FeedRetentionRuleExtended.cs
@@ -1,6 +1,21 @@
-﻿namespace ProGet.Net.Native.Models
+﻿// ReSharper disable InconsistentNaming
+
+namespace ProGet.Net.Native.Models
 {
     public class FeedRetentionRuleExtended
     {
+        public int Feed_Id { get; set; }
+        public string Feed_Name { get; set; }
+        public string FeedType_Name { get; set; }
+        public int Sequence_Number { get; set; }
+        public bool DeletePrereleaseVersions_Indicator { get; set; }
+        public int KeepVersions_Count { get; set; }
+        public object KeepUsedWithin_Days { get; set; }
+        public object DeletePackageIds_Csv { get; set; }
+        public object KeepPackageIds_Csv { get; set; }
+        public bool DeleteCached_Indicator { get; set; }
+        public int SizeTrigger_KBytes { get; set; }
+        public bool SizeExclusive_Indicator { get; set; }
+        public object TriggerDownload_Count { get; set; }
     }
 }
---
+++ test/ProGet.Net.Tests/Native/FeedRetentionRules/ProGetClientShould.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace ProGet.Net.Tests
+{
+    public partial class ProGetClientShould
+    {
+        [Fact]
+        public async Task FeedRetentionRules_GetRulesAsync()
+        {
+            var results = await _client.FeedRetentionRules_GetRulesAsync(2);
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+    }
+}
+
