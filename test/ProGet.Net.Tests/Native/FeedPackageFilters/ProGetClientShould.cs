76d9358bf81b6a631b9cc0d85d3c6bdb05c8d957
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-06-11 07:18:16 +0200

    add feed package filter tests

---
+++ test/ProGet.Net.Tests/Native/FeedPackageFilters/ProGetClientShould.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace ProGet.Net.Tests
+{
+    public partial class ProGetClientShould
+    {
+        [Fact]
+        public async Task FeedPackageFilters_GetPackageFiltersAsync()
+        {
+            var results = await _client.FeedPackageFilters_GetPackageFiltersAsync(2);
+
+            Assert.NotNull(results);
+            //Assert.NotEmpty(results);
+        }
+    }
+}
+
