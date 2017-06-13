b93e628f636272dc5a3026238ef5d8fb2fdec77e
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-06-13 12:27:06 +0200

    add docker image tests

---
+++ test/ProGet.Net.Tests/Native/DockerImages/ProGetClientShould.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace ProGet.Net.Tests
+{
+    public partial class ProGetClientShould
+    {
+        [Fact]
+        public async Task DockerImages_GetImagesAsync()
+        {
+            var results = await _client.DockerImages_GetImagesAsync(6, null);
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+
+        [Fact]
+        public async Task DockerImages_GetRepositoriesAsync()
+        {
+            var results = await _client.DockerImages_GetRepositoriesAsync(6, null);
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+
+        [Fact]
+        public async Task DockerImages_GetTagsAsync()
+        {
+            var results = await _client.DockerImages_GetTagsAsync(6, null);
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+    }
+}
+
