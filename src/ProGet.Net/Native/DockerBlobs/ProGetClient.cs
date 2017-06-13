c6aafc5d42a1916d4a6e8e97b531d8a9d72bcd56
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-06-13 12:02:11 +0200

    add DockerBlobs_* (empty models)

--- src/ProGet.Net/Native/DockerBlobs/ProGetClient.cs
+++ src/ProGet.Net/Native/DockerBlobs/ProGetClient.cs
@@ -10,5 +10,16 @@ namespace ProGet.Net
 {
     public partial class ProGetClient
     {
+        /// <summary>
+        /// Gets all of the blobs in a feed
+        /// </summary>
+        public async Task<IEnumerable<DockerBlob>> DockerBlobs_GetBlobsAsync(int feed_Id)
+        {
+            var queryParamValues = QueryParamValues.From(
+                new NamedValue(nameof(feed_Id).Capitalize(), feed_Id)
+            );
+
+            return await ExecuteNativeApiMethodAsync<IEnumerable<DockerBlob>>(nameof(DockerBlobs_GetBlobsAsync).WithoutAsyncSuffix(), queryParamValues).ConfigureAwait(false);
+        }
     }
 }
