cb0c7e6e8662852d405c92f596278677a27735e6
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-06-13 09:47:01 +0200

    add npm feed models

--- src/ProGet.Net/Native/Models/NpmPackageAllVersions.cs
+++ src/ProGet.Net/Native/Models/NpmPackageAllVersions.cs
@@ -1,6 +1,19 @@
-﻿namespace ProGet.Net.Native.Models
+﻿// ReSharper disable InconsistentNaming
+
+namespace ProGet.Net.Native.Models
 {
     public class NpmPackageAllVersions
     {
+        public int Feed_Id { get; set; }
+        public string Package_Name { get; set; }
+        public string Scope_Name { get; set; }
+        public string Version_Text { get; set; }
+        public string Published_Date { get; set; }
+        public int Download_Count { get; set; }
+        public object LastDownloaded_Date { get; set; }
+        public string PackageHash_Bytes { get; set; }
+        public string PackageJson_Bytes { get; set; }
+        public bool Cached_Indicator { get; set; }
+        public int Package_Size { get; set; }
     }
 }
---
+++ test/ProGet.Net.Tests/Native/NpmFeeds/ProGetClientShould.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace ProGet.Net.Tests
+{
+    public partial class ProGetClientShould
+    {
+        [Fact]
+        public async Task NpmFeeds_GetAllPackageVersionsAsync()
+        {
+            var results = await _client.NpmFeeds_GetAllPackageVersionsAsync(4);
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+
+        [Fact]
+        public async Task NpmFeeds_GetConnectorPackageAsync()
+        {
+            var result = await _client.NpmFeeds_GetConnectorPackageAsync(4, "express", "4.15.3");
+
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task NpmFeeds_GetLatestPackageVersionsAsync()
+        {
+            var results = await _client.NpmFeeds_GetLatestPackageVersionsAsync(4, DateTime.Today.AddYears(-2));
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+    }
+}
+
