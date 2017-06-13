5243a261ec7de680bf334cb52f85f9d6c9914b33
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-06-13 08:50:29 +0200

    add NpmConnectors_* (empty models)

---
+++ src/ProGet.Net/Native/NpmConnectors/ProGetClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ProGet.Net.Common;
+using ProGet.Net.Native.Models;
+
+// ReSharper disable InconsistentNaming
+// ReSharper disable CheckNamespace
+
+namespace ProGet.Net
+{
+    public partial class ProGetClient
+    {
+        /// <summary>
+        /// Gets the npm connector packages which match the specified criteria
+        /// </summary>
+        public async Task<IEnumerable<NpmConnectorPackage>> NpmConnectors_GetPackagesAsync(
+            int connector_Id,
+            string includeName_Csv,
+            string excludeName_Csv,
+            DateTime modifiedAfter_Date,
+            int maxResults_Count)
+        {
+            var queryParamValues = QueryParamValues.From(
+                new NamedValue(nameof(connector_Id).Capitalize(), connector_Id),
+                new NamedValue(nameof(includeName_Csv).Capitalize(), includeName_Csv),
+                new NamedValue(nameof(excludeName_Csv).Capitalize(), excludeName_Csv),
+                new NamedValue(nameof(modifiedAfter_Date).Capitalize(), modifiedAfter_Date), //TODO: verify DateTime
+                new NamedValue(nameof(maxResults_Count).Capitalize(), maxResults_Count)
+            );
+
+            return await ExecuteNativeApiMethodAsync<IEnumerable<NpmConnectorPackage>>(nameof(NpmConnectors_GetPackagesAsync).WithoutAsyncSuffix(), queryParamValues).ConfigureAwait(false);
+        }
+    }
+}
