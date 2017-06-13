fe12164cd0dd6ee494a42ee7d46e019ab94790c4
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-06-13 12:05:19 +0200

    add docker blob tests

---
+++ test/ProGet.Net.Tests/Native/DockerBlobs/ProGetClientShould.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace ProGet.Net.Tests
+{
+    public partial class ProGetClientShould
+    {
+        [Fact]
+        public async Task DockerBlobs_GetBlobsAsync()
+        {
+            var results = await _client.DockerBlobs_GetBlobsAsync(6);
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+    }
+}
+
