b6c45de7257580efe0c1bf7e0f4bf0ae59dd490d
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-06-13 10:12:44 +0200

    add npm packages tests

---
+++ test/ProGet.Net.Tests/Native/NpmPackages/ProGetClientShould.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace ProGet.Net.Tests
+{
+    public partial class ProGetClientShould
+    {
+        [Fact]
+        public async Task NpmPackages_GetPackageAsync()
+        {
+            var result = await _client.NpmPackages_GetPackageAsync(4, "express", "latest");
+
+            Assert.NotNull(result);
+        }
+    }
+}
+
