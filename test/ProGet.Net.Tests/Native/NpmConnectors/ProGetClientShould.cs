2b7c107fc3ab6ac6f22542f76931b5c2ad9bad11
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-06-13 09:03:42 +0200

    add npm connector tests

---
+++ test/ProGet.Net.Tests/Native/NpmConnectors/ProGetClientShould.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace ProGet.Net.Tests
+{
+    public partial class ProGetClientShould
+    {
+        [Fact]
+        public async Task NpmConnectors_GetPackagesAsync()
+        {
+            var results = await _client.NpmConnectors_GetPackagesAsync(2, "express", "", DateTime.Today.AddYears(-2), 10);
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+    }
+}
+
