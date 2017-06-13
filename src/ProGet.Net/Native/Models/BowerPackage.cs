c533e83bd67c5de4ae6f2e3aac3a60582db39c1e
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-06-13 11:55:47 +0200

    add bower package models

--- src/ProGet.Net/Native/Models/BowerPackage.cs
+++ src/ProGet.Net/Native/Models/BowerPackage.cs
@@ -1,6 +1,13 @@
-﻿namespace ProGet.Net.Native.Models
+﻿// ReSharper disable InconsistentNaming
+
+namespace ProGet.Net.Native.Models
 {
     public class BowerPackage
     {
+        public int Feed_Id { get; set; }
+        public string Package_Name { get; set; }
+        public string Repository_Url { get; set; }
+        public int Hit_Count { get; set; }
+        public object LastRequested_Date { get; set; }
     }
 }
---
+++ test/ProGet.Net.Tests/Native/BowerPackages/ProGetClientShould.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+// ReSharper disable CheckNamespace
+
+namespace ProGet.Net.Tests
+{
+    public partial class ProGetClientShould
+    {
+        [Fact]
+        public async Task BowerPackages_GetPackagesAsync()
+        {
+            var results = await _client.BowerPackages_GetPackagesAsync(5);
+
+            Assert.NotNull(results);
+            Assert.NotEmpty(results);
+        }
+    }
+}
+
