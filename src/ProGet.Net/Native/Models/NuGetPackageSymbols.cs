b58bb3cbbdc0cce3e2f49d699cf9c2005c21f460
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-02 18:11:20 +0200

    add missing model: NuGetPackageSymbols

--- src/ProGet.Net/Native/Models/NuGetPackageSymbols.cs
+++ src/ProGet.Net/Native/Models/NuGetPackageSymbols.cs
@@ -1,6 +1,15 @@
-﻿namespace ProGet.Net.Native.Models
+﻿// ReSharper disable InconsistentNaming
+
+namespace ProGet.Net.Native.Models
 {
     public class NuGetPackageSymbols
     {
+        public int Feed_Id { get; set; }
+        public byte[] Symbol_Id { get; set; }
+        public string SymbolFileName_Text { get; set; }
+        public int Symbol_Age { get; set; }
+        public string Package_Id { get; set; }
+        public string Package_Version_Text { get; set; }
+        public string Package_SymbolFilePath_Text { get; set; }
     }
 }
