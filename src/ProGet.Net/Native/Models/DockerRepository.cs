55654ac1b014c3f7d12616563a5e17ecb9160e70
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-02 18:18:01 +0200

    add missing model: DockerRepository

--- src/ProGet.Net/Native/Models/DockerRepository.cs
+++ src/ProGet.Net/Native/Models/DockerRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace ProGet.Net.Native.Models
+﻿using System;
+
+// ReSharper disable InconsistentNaming
+
+namespace ProGet.Net.Native.Models
 {
     public class DockerRepository
     {
+        public int Feed_Id { get; set; }
+        public string Repository_Name { get; set; }
+        public int? Download_Count { get; set; }
+        public DateTime? Updated_Date { get; set; }
+        public int? Image_Count { get; set; }
+        public string ShortDescription_Text { get; set; }
+        public string FullDescription_Text { get; set; }
     }
 }
