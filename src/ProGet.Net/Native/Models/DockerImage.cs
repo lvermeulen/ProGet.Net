474e3e50a2e4efc12bd087f89404aeb6027c595a
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-02 18:17:41 +0200

    add missing model: DockerImage

--- src/ProGet.Net/Native/Models/DockerImage.cs
+++ src/ProGet.Net/Native/Models/DockerImage.cs
@@ -1,6 +1,16 @@
-﻿namespace ProGet.Net.Native.Models
+﻿using System;
+
+// ReSharper disable InconsistentNaming
+
+namespace ProGet.Net.Native.Models
 {
     public class DockerImage
     {
+        public int Feed_Id { get; set; }
+        public string Repository_Name { get; set; }
+        public string Image_Digest { get; set; }
+        public byte[] ManifestJson_Bytes { get; set; }
+        public int Download_Count { get; set; }
+        public DateTime Published_Date { get; set; }
     }
 }
