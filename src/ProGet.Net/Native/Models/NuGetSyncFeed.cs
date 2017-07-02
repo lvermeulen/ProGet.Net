ba0ed86671e57825b2d51fdbe09e62a7dcf38f89
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-02 18:11:39 +0200

    add missing model: NuGetSyncFeed

--- src/ProGet.Net/Native/Models/NuGetSyncFeed.cs
+++ src/ProGet.Net/Native/Models/NuGetSyncFeed.cs
@@ -1,6 +1,17 @@
-﻿namespace ProGet.Net.Native.Models
+﻿using System;
+
+// ReSharper disable InconsistentNaming
+
+namespace ProGet.Net.Native.Models
 {
     public class NuGetSyncFeed
     {
+        public int Feed_Id { get; set; }
+        public string Sync_Url { get; set; }
+        public string RemoteFeed_Name { get; set; }
+        public byte[] RemoteFeed_APIKey_Bytes { get; set; }
+        public int Sync_Frequency_Minutes { get; set; }
+        public bool Active_Indicator { get; set; }
+        public DateTime? Last_Sync_Date { get; set; }
     }
 }
