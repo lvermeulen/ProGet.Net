642510422da980aead806a21e1c05d8c38cf9b2e
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-02 18:04:08 +0200

    add missing model: NpmConnectorPackage

--- src/ProGet.Net/Native/Models/NpmConnectorPackage.cs
+++ src/ProGet.Net/Native/Models/NpmConnectorPackage.cs
@@ -1,6 +1,15 @@
-﻿namespace ProGet.Net.Native.Models
+﻿using System;
+
+// ReSharper disable InconsistentNaming
+
+namespace ProGet.Net.Native.Models
 {
     public class NpmConnectorPackage
     {
+        public int Connector_Id { get; set; }
+        public string Package_Name { get; set; }
+        public string Package_Version { get; set; }
+        public DateTime Modified_Date { get; set; }
+        public byte[] PackageJson_Bytes { get; set; }
     }
 }
