cf9547dd36ecbff249014e91811e3009b24fde10
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-02 18:17:51 +0200

    add missing model: DockerImageTag

--- src/ProGet.Net/Native/Models/DockerImageTag.cs
+++ src/ProGet.Net/Native/Models/DockerImageTag.cs
@@ -1,6 +1,12 @@
-﻿namespace ProGet.Net.Native.Models
+﻿// ReSharper disable InconsistentNaming
+
+namespace ProGet.Net.Native.Models
 {
     public class DockerImageTag
     {
+        public int Feed_Id { get; set; }
+        public string Repository_Name { get; set; }
+        public string Tag_Name { get; set; }
+        public string Image_Digest { get; set; }
     }
 }
