0024edb10802ef4fc752d47ae23893fa3749f5a7
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-02 18:02:24 +0200

    add missing model: NuGetPackagesExtendedNuGetPackageSymbols

--- src/ProGet.Net/Native/Models/NuGetPackagesExtendedNuGetPackageSymbols.cs
+++ src/ProGet.Net/Native/Models/NuGetPackagesExtendedNuGetPackageSymbols.cs
@@ -1,6 +1,12 @@
-﻿namespace ProGet.Net.Native.Models
+﻿using System.Collections.Generic;
+
+// ReSharper disable InconsistentNaming
+
+namespace ProGet.Net.Native.Models
 {
     public class NuGetPackagesExtendedNuGetPackageSymbols
     {
+        public List<NuGetPackageExtended> NuGetPackages_Extended { get; set; }
+        public List<NuGetPackageSymbols> NuGetPackageSymbols { get; set; }
     }
 }
