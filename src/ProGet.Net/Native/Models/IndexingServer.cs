328345a3267de870b8d9e8c7ee4a98870cac6a31
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-02 18:03:57 +0200

    add missing model: IndexingServer

--- src/ProGet.Net/Native/Models/IndexingServer.cs
+++ src/ProGet.Net/Native/Models/IndexingServer.cs
@@ -1,6 +1,14 @@
-﻿namespace ProGet.Net.Native.Models
+﻿using System;
+
+// ReSharper disable InconsistentNaming
+
+namespace ProGet.Net.Native.Models
 {
     public class IndexingServer
     {
+        public string Server_Name { get; set; }
+        public string IPAddress_Text { get; set; }
+        public DateTime LastUpdated_Date { get; set; }
+        public bool Primary_Indicator { get; set; }
     }
 }
