dac0ed5e8df83aac035dc3b21f76554fd738f9d1
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-02 18:03:26 +0200

    add missing model: FeedAdapter

--- src/ProGet.Net/Native/Models/FeedAdapter.cs
+++ src/ProGet.Net/Native/Models/FeedAdapter.cs
@@ -1,6 +1,13 @@
-﻿namespace ProGet.Net.Native.Models
+﻿// ReSharper disable InconsistentNaming
+
+namespace ProGet.Net.Native.Models
 {
     public class FeedAdapter
     {
+        public int Feed_Id { get; set; }
+        public int FeedAdapter_Sequence { get; set; }
+        public string FeedAdapter_Name { get; set; }
+        public string FeedAdapterType_Code { get; set; }
+        public string FeedAdapter_Configuration { get; set; }
     }
 }
