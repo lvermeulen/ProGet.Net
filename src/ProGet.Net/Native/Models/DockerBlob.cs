891d3eb463b655ca0a1250dc783ffeab60af95b5
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-02 18:17:29 +0200

    add missing model: DockerBlob

--- src/ProGet.Net/Native/Models/DockerBlob.cs
+++ src/ProGet.Net/Native/Models/DockerBlob.cs
@@ -1,6 +1,15 @@
-﻿namespace ProGet.Net.Native.Models
+﻿using System;
+
+// ReSharper disable InconsistentNaming
+
+namespace ProGet.Net.Native.Models
 {
     public class DockerBlob
     {
+        public int Feed_Id { get; set; }
+        public string Blob_Digest { get; set; }
+        public int Download_Count { get; set; }
+        public DateTime? LastRequested_Date { get; set; }
+        public long Blob_Size { get; set; }
     }
 }
