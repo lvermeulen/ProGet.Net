2b392ed03ad2ae3fdfaa827fdf077504e7040839
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-02 18:11:56 +0200

    add missing model: ProGetPackage

--- src/ProGet.Net/Native/Models/ProGetPackage.cs
+++ src/ProGet.Net/Native/Models/ProGetPackage.cs
@@ -1,6 +1,17 @@
-﻿namespace ProGet.Net.Native.Models
+﻿// ReSharper disable InconsistentNaming
+
+namespace ProGet.Net.Native.Models
 {
     public class ProGetPackage
     {
+        public int ProGetPackage_Id { get; set; }
+        public int Feed_Id { get; set; }
+        public string Group_Name { get; set; }
+        public string Package_Name { get; set; }
+        public string Title_Text { get; set; }
+        public string Description_Text { get; set; }
+        public string IconUrl_Text { get; set; }
+        public string LatestVersion_Text { get; set; }
+        public int Download_Count { get; set; }
     }
 }
