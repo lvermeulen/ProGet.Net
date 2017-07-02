96c6d4c639a3bcf1c82d88e28fb1ad20e63ae746
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-02 18:12:15 +0200

    add missing model: ProGetPackageVersionExtended

--- src/ProGet.Net/Native/Models/ProGetPackageVersionExtended.cs
+++ src/ProGet.Net/Native/Models/ProGetPackageVersionExtended.cs
@@ -1,6 +1,20 @@
-﻿namespace ProGet.Net.Native.Models
+﻿using System;
+
+// ReSharper disable InconsistentNaming
+
+namespace ProGet.Net.Native.Models
 {
     public class ProGetPackageVersionExtended
     {
+        public int ProGetPackage_Id { get; set; }
+        public string Package_Name { get; set; }
+        public string Group_Name { get; set; }
+        public string Version_Text { get; set; }
+        public byte[] PackageHash_Bytes { get; set; }
+        public byte[] PackageMetadata_Bytes { get; set; }
+        public DateTime Published_Date { get; set; }
+        public long Package_Size { get; set; }
+        public int Download_Count { get; set; }
+        public bool Cached_Indicator { get; set; }
     }
 }
