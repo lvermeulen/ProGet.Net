5de271b25f8aba5ec4e3ffe63f5be6031a255456
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-07-02 18:03:40 +0200

    add missing model: FeedPackageFilter

--- src/ProGet.Net/Native/Models/FeedPackageFilter.cs
+++ src/ProGet.Net/Native/Models/FeedPackageFilter.cs
@@ -1,6 +1,12 @@
-﻿namespace ProGet.Net.Native.Models
+﻿// ReSharper disable InconsistentNaming
+
+namespace ProGet.Net.Native.Models
 {
     public class FeedPackageFilter
     {
+        public int Feed_Id { get; set; }
+        public int Sequence_Number { get; set; }
+        public string PackageFilter_Name { get; set; }
+        public string PackageFilter_Configuration { get; set; }
     }
 }
